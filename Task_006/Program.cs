599508d0ee722f22f287343555d746d1b88e3a04
ritesu <ritesu@mail.ru>
2023-02-09 10:25:45 +1100

    Task_006

---
+++ Task_006/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите размеры трехмерного массива");
+int a = int.Parse(Console.ReadLine() ?? "");
+int b = int.Parse(Console.ReadLine() ?? "");
+int c = int.Parse(Console.ReadLine() ?? "");
+int[,,] numbers = new int[a, b, c];
+
+void FillNumbers(int[,,] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            for (int k = 0; k < numbers.GetLength(2); k++)
+            {
+                numbers[i,j,k] = 10 + count;
+                count++;
+            }
+
+        }
+
+    }
+}
+
+void PrintNumbers(int[,,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            for (int k = 0; k < numbers.GetLength(2); k++)
+            {
+                Console.Write(numbers[i, j, k] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+FillNumbers(numbers);
+PrintNumbers(numbers);
