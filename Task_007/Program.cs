5564abc00bb52c5ed4baae69f9bed70e9c272ce8
ritesu <ritesu@mail.ru>
2023-02-10 19:10:35 +1100

    Task_007

---
+++ Task_007/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите количество строк Треугольника Паскаля, которые надо показать");
+int a = int.Parse(Console.ReadLine() ?? "");
+int i = 0;
+int b = 0;
+static float Factorial(int a)
+{
+    float i, x = 1;
+    for (i = 1; i <= a; i++)
+    {
+        x *= i;
+    }
+    return x;
+}
+void PrintTriPas()
+{
+    for (i = 0; i < a; i++)
+    {
+        for (b = 0; b <= (a - i); b++)
+        {
+            Console.Write(" ");
+        }
+        for (b = 0; b <= i; b++)
+        {
+            Console.Write(" ");
+            Console.Write(Factorial(i) / (Factorial(b) * Factorial(i - b)));
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+PrintTriPas();
